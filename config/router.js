453b88cde78d13aec39678423cbea47bfb68e2da
James Guppy <j.guppy321@gmail.com>
2021-08-24 15:10:20 +0100

    Made progress on user permisisons. Admins can now delete and edit both clubs and pubs. Users can delete their own pubs. Other users and non-registered users should receive an Unauthorized error message when trying to attempt these. Next steps are do the same for the comments and get the addedBy info for things generated in the seed function.

--- config/router.js
+++ config/router.js
@@ -9,21 +9,18 @@ const router = express.Router()
 router.route('/clubs')
   .get(clubs.index)
   .post(secureRoute, clubs.create)
-  .post(clubs.create)
 
 router.route('/clubs/:clubId')
   .get(clubs.show)
   .delete(secureRoute, clubs.delete)
   .put(secureRoute, clubs.update)
 
-  .put(clubs.update)
-
 // * PUBS
 router.route('/clubs/:clubId/pubs')
-  .post(clubs.pubCreate)
+  .post(secureRoute, clubs.pubCreate)
 
 router.route('/clubs/:clubId/pubs/:pubId')
-  .delete(clubs.pubDelete) 
+  .delete(secureRoute, clubs.pubDelete) 
 
 // * COMMENTS
 router.route('/clubs/:clubId/pubs/:pubId')
